c481a9df8ef642bd9d71a5c217a206f701433956
Gustavo da Hora <gustavo.carmodahora@outlook.com>
2022-05-13 11:07:37 -0300

    Creating a layout to github profiles

--- app/src/main/java/dev/gustavodahora/githubprofiles/ui/home/ProfilesFragment.kt
+++ app/src/main/java/dev/gustavodahora/githubprofiles/ui/home/ProfilesFragment.kt
@@ -4,14 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import dev.gustavodahora.githubprofiles.databinding.FragmentHomeBinding
+import dev.gustavodahora.githubprofiles.databinding.FragmentProfilesBinding
 
-class HomeFragment : Fragment() {
+class ProfilesFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
+    private var _binding: FragmentProfilesBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -23,15 +22,15 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+            ViewModelProvider(this).get(ProfilesViewModel::class.java)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentProfilesBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+//        val textView: TextView = binding.textHome
+//        homeViewModel.text.observe(viewLifecycleOwner) {
+//            textView.text = it
+//        }
         return root
     }
 
