7eab420a2d0c4981069a0089774b825a7a83f42a
Gustavo da Hora <gustavo.carmodahora@outlook.com>
2022-05-13 11:34:05 -0300

    Adding card layout.

--- app/src/main/java/dev/gustavodahora/githubprofiles/ui/home/ProfilesFragment.kt
+++ app/src/main/java/dev/gustavodahora/githubprofiles/ui/home/ProfilesFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.textfield.TextInputLayout
 import dev.gustavodahora.githubprofiles.databinding.FragmentProfilesBinding
 
 class ProfilesFragment : Fragment() {
@@ -31,7 +33,10 @@ class ProfilesFragment : Fragment() {
 //        homeViewModel.text.observe(viewLifecycleOwner) {
 //            textView.text = it
 //        }
-        return root
+
+        val editUsername: TextInputLayout = binding.editUsername
+
+        return binding.root
     }
 
     override fun onDestroyView() {
